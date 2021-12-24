f942e07869d13da1737a696514df388bea913807
Manohar <manoharramarao@gmail.com>
2021-12-24 21:06:06 +0530

    Created Buttons Added Buttons to the theme Created customized button widgets

---
+++ lib/d_widgets/d_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class DColors {
+  static const Color primaryColor = Color(0xff26890D);
+  static const Color secondaryColor = Color(0xff86BC25);
+  static const Color disabledColor = Color(0xff2C2C2C);
+}
--- lib/screens/buttons.dart
+++ lib/screens/buttons.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/d_widgets/d_button.dart';
 import 'package:myapp/d_widgets/leading_icon.dart';
 
 class DButtons extends StatelessWidget {
@@ -10,6 +11,42 @@ class DButtons extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Buttons"),
         leading: const DBackButton(),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            DTextButton(
+              onPressed: () {},
+              text: "submit",
+              block: false,
+            ),
+            DTextButton(
+              onPressed: () {},
+              text: "block button",
+              block: true,
+            ),
+            DTextButton(
+              onPressed: () {},
+              text: "Secondary Button",
+              type: "secondary",
+            ),
+            DTextButton(
+              onPressed: () {},
+              text: "Action Button",
+              type: "action",
+            ),
+            const DTextButton(
+              onPressed: null,
+              text: "I am disabled",
+              block: true,
+            ),
+            const TextButton(
+              onPressed: null,
+              child: Text("SUBMIT"),
+            )
+          ],
+        ),
       ),
     );
   }
