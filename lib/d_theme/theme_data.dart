8722edb4e0a50a6e77604a89107281889c93a268
Manohar <manoharramarao@gmail.com>
2021-12-24 13:12:51 +0530

    Theme Updated Changed fonts to Open sans

--- lib/d_theme/theme_data.dart
+++ lib/d_theme/theme_data.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DTheme {
   static const _appbarBottomBorderColor = 0xff424242;
 
   static ThemeData get darkTheme {
-    return ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Colors.black,
-        buttonTheme: ThemeData.dark().buttonTheme.copyWith(
+    // * not extending dart theme data because textTheme needs to be changed
+    return ThemeData(
+      textTheme: GoogleFonts.openSansTextTheme(
+        ThemeData.dark().textTheme,
+      ),
+      // !Overriding icon theme is not working
+      iconTheme: const IconThemeData(
+        color: Colors.green,
+      ),
+      scaffoldBackgroundColor: Colors.black,
+      buttonTheme: ThemeData.dark().buttonTheme.copyWith(
             colorScheme: const ColorScheme.dark()
-                .copyWith(background: Colors.green, primary: Colors.green)),
-        appBarTheme: ThemeData.dark().appBarTheme.copyWith(
+                .copyWith(background: Colors.green, primary: Colors.green),
+          ),
+      appBarTheme: ThemeData.dark().appBarTheme.copyWith(
             centerTitle: true,
             backgroundColor: Colors.black,
             shape: const Border(
-                bottom: BorderSide(color: Color(_appbarBottomBorderColor)))));
+              bottom: BorderSide(
+                color: Color(_appbarBottomBorderColor),
+              ),
+            ),
+          ),
+    );
   }
 }
