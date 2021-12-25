6a8ca536c09099b872a82544c6b7b00c243f7ad2
Manohar <manoharramarao@gmail.com>
2021-12-25 11:20:23 +0530

    Created separate home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/d_theme/theme_data.dart';
-import 'package:myapp/screens/buttons.dart';
-import 'package:myapp/screens/typography.dart';
+import 'package:myapp/screens/home.dart';
 
 void main() {
-  runApp(MaterialApp(home: const HomePage(), theme: DTheme.darkTheme));
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("My Style Guide"),
-      ),
-      body: ListView(
-        children: [
-          ListTile(
-            title: const Text(
-              "Typography",
-            ),
-            trailing: const Icon(
-              Icons.arrow_forward_ios_rounded,
-              color: Colors.white, // ! this should come from iconTheme data
-            ),
-            enabled: true,
-            onTap: () => navigateToTypography(context),
-          ),
-          ListTile(
-            title: const Text(
-              "Buttons",
-            ),
-            trailing: const Icon(
-              Icons.arrow_forward_ios_rounded,
-              color: Colors.white, // ! this should come from iconTheme data
-            ),
-            enabled: true,
-            onTap: () => navigateToButtons(context),
-          ),
-        ],
-      ),
-    );
-  }
-
-  navigateToTypography(context) {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => const DTypography()));
-  }
-
-  navigateToButtons(context) {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => const DButtons()));
-  }
+  runApp(MaterialApp(home: const HomeScreen(), theme: DTheme.darkTheme));
 }
