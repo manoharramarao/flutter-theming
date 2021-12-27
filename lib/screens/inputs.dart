fadffd26b0ef117f8ff7f90d72be0f472975295b
Manohar <manoharramarao@gmail.com>
2021-12-27 15:05:49 +0530

    Created text and dropdown inputs added validations dropdown is built using dropdown_search plugin

--- lib/screens/inputs.dart
+++ lib/screens/inputs.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/d_theme/d_widgets/d_dropdown.dart';
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:dio/dio.dart';
 
-class Dinputs extends StatelessWidget {
-  const Dinputs({Key? key}) : super(key: key);
+/* class Dinputs extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  
+  Dinputs({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -10,32 +14,99 @@ class Dinputs extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Inputs"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(3.0),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(3.0),
+                    ),
                   ),
+                  hintText: "example@deloitte.com",
+                  labelText: "User ID",
                 ),
-                hintText: "example@deloitte.com",
-                labelText: "User ID",
+                validator: (text) {
+                  if (text == null || text.isEmpty) {
+                    return 'Text is empty';
+                  }
+                  return null;
+                },
               ),
-            ),
-            DropdownButton<String>(
-              items: <String>['A', 'B', 'C', 'D'].map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (_) {},
-            ),
-            const DDropDown(),
-          ],
+              DropdownButton<String>(
+                items: <String>['A', 'B', 'C', 'D'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (_) {},
+              ),
+              const DDropdown(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+} */
+
+class DInputs extends StatefulWidget {
+  const DInputs({Key? key}) : super(key: key);
+
+  @override
+  _DInputsState createState() => _DInputsState();
+}
+
+class _DInputsState extends State<DInputs> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Inputs"),
+      ),
+      body: Form(
+        key: _formKey,
+        // autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(3.0),
+                    ),
+                  ),
+                  hintText: "example@deloitte.com",
+                  labelText: "User ID",
+                ),
+                validator: (text) {
+                  if (text == null || text.isEmpty) {
+                    return 'Text is empty';
+                  }
+                  return null;
+                },
+              ),
+              DropdownButton<String>(
+                items: <String>['A', 'B', 'C', 'D'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (_) {},
+              ),
+              const DDropdown(),
+            ],
+          ),
         ),
       ),
     );
